0d9bb056d0e18f7318533f5c09cf6e29be760fec
madonuko <madonuko@outlook.com>
2025-03-10 16:18:08 +0800

    feat(exec)!: cd to the current directory

--- src/exec.nim
+++ src/exec.nim
@@ -2,22 +2,26 @@ import common_imports
 import_macros
 import nimsutils
 
-proc run*(cmd: string; quiet=false): tuple[output: string, exitCode: int] {.discardable.} =
+proc run*(
+    cmd: string, quiet = false
+): tuple[output: string, exitCode: int] {.discardable.} =
   if not quiet:
     hint " $ " & cmd
   try:
-    result = gorgeEx cmd
+    let cwd = getCurrentDir()
+    result = gorgeEx "cd " & cwd & " && " & cmd
     if result.exitCode != 0:
       echo fmt"┌─ Fail to execute command: {cmd}"
       for line in result.output.splitLines:
-        echo "│ "&line
+        echo "│ " & line
       echo fmt"└─ Command returned exit code {$result.exitCode}"
       quit 1
   except CatchableError as e:
     echo fmt"Fail to execute command: {cmd}"
     raise e
 
-proc run_quiet*(cmd: string): tuple[output: string, exitCode: int] {.discardable.} = run(cmd, true)
+proc run_quiet*(cmd: string): tuple[output: string, exitCode: int] {.discardable.} =
+  run(cmd, true)
 
 macro sh*(body: untyped): tuple[output: string, exitCode: int] {.discardable.} =
   body.expectKind nnkStmtList
